d34131462972e3a44c8673c9bdb59953489c2084
bebelmax <bebelmax@bszwidv.by.lo-net2.de>
2014-11-04 11:11:54 +0100

    added cleanUpFp

---
+++ ActiveDirectory/cleanupFpaA.ps1
@@ -0,0 +1,24 @@
+$path = "E:\Temp\" 
+$now = get-Date
+
+$raum = read-Host "Raum: "
+$Computers = Get-Content "E:\Temp\Computerräume.txt"
+#	
+$answer = read-Host -Prompt "Start cleanup (y/n)? "
+
+if($answer -eq "y") {
+	write-Host "... cleanup running, please standby!"
+
+	foreach ($Computer in $Computers) {
+		set-Location -Path ("E:\Benutzer\fpA$\" + $Computer + "\Eigene Dateien") -ErrorAction "SilentlyContinue"
+		
+		$files = gci -recurse
+		foreach($file in $files) {
+			$timespan = new-Timespan -Start $file.LastWriteTime -End $now;	
+			if($timespan.Days -gt 14) {
+				remove-Item $file.FullName -recurse -ErrorAction "SilentlyContinue"
+			}
+		}
+	}
+	set-Location E:\Admin\Powershell
+}
