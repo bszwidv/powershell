af7d7815c785ffca452ff0ac912037b2c4c99fd3
U9dc1 <support@dv-schulen.de>
2014-11-07 11:49:39 +0100

    Revert "visual changes"
    This reverts commit 5efa9242ab83c47843f4fc87451fdd44babdcd62.

--- ActiveDirectory/cleanupFpaA.ps1
+++ ActiveDirectory/cleanupFpaA.ps1
@@ -7,25 +7,19 @@ $Computers = Get-Content "E:\Temp\Computerräume\$datei"
 #	
 $answer = read-Host -Prompt "Start cleanup (y/n)? "
 
-if($answer -eq "y") 
-{
+if($answer -eq "y") {
 	write-Host "... cleanup running, please standby!"
-	foreach ($Computer in $Computers) 
-    {
+
+	foreach ($Computer in $Computers) {
 		set-Location -Path ("E:\Benutzer\fpA$\" + $Computer + "\Eigene Dateien") -ErrorAction "SilentlyContinue"
 		
 		$files = gci -recurse
-		foreach($file in $files) 
-        {
-		    $timespan = new-Timespan -Start $file.LastWriteTime -End $now;	
-		    if($timespan.Days -gt 14) 
-            {
-		    remove-Item $file.FullName -recurse -ErrorAction "SilentlyContinue"
-		    }
+		foreach($file in $files) {
+			$timespan = new-Timespan -Start $file.LastWriteTime -End $now;	
+			if($timespan.Days -gt 14) {
+				remove-Item $file.FullName -recurse -ErrorAction "SilentlyContinue"
+			}
 		}
-	 }	
-}
-else
-{
-    write-host "Cancelled"
+	}
+	set-Location E:\Admin\Powershell
 }
